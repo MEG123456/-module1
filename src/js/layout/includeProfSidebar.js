ccecad047ce7b889e1a4e5f8c7db4e1271e87b89
aida030313-coder <aida030313@g.eulji.ac.kr>
2026-03-19 17:01:56 +0900

    사이드바 수정

--- src/js/layout/includeProfSidebar.js
+++ src/js/layout/includeProfSidebar.js
@@ -2,4 +2,17 @@ fetch("/layout/profSidebar.html")
   .then((res) => res.text())
   .then((html) => {
     document.getElementById("sidebar").innerHTML = html;
-  });
+
+  const currentPath = window.location.pathname;
+    const menuLinks = document.querySelectorAll('.student-sidebar li a');
+
+    menuLinks.forEach(link => {
+        const linkHref = link.getAttribute('href').split('/').pop();
+        const currentFile = currentPath.split('/').pop();
+
+        if (linkHref === currentFile) {
+            link.classList.add('active');
+        }
+    });
+  })
+  .catch(error => console.error('사이드바 로드 실패:', error));
