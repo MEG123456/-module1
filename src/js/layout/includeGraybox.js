0739e5707e210f39fce035407d68b65b7b2780d8
always62442 <always22@g.eulji.ac.kr>
2026-03-19 15:53:30 +0900

    학생 강의 목록 교수 목록과 연결

--- src/js/layout/includeGraybox.js
+++
@@ -1,5 +0,0 @@
-fetch("/layout/grayBox.html")
-  .then((response) => response.text())
-  .then((responseText) => {
-    document.querySelector(".container-body").insertAdjacentHTML("afterbegin", responseText);
-  })
