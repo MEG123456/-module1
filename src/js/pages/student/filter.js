63d6d81c40d7a9103d4d36d5b5f7f9778484735b
always62442 <always22@g.eulji.ac.kr>
2026-03-25 11:15:31 +0900

    강의 더미데이터 충돌 해결 중

--- src/js/pages/student/filter.js
+++ src/js/pages/student/filter.js
@@ -47,10 +47,17 @@ class LectureManager {
 
     // [수정] 임의 데이터 삭제, 로컬 스토리지에서만 로드
     loadLocalData() {
-    const data = JSON.parse(localStorage.getItem("lectures_all")) || [];
+    let allLectures = [];
 
-    this.allLectures = data;
-    this.displayLectures = [...data];
+    Object.keys(localStorage).forEach(key => {
+        if (key.startsWith("lectures_")) {
+            const data = JSON.parse(localStorage.getItem(key)) || [];
+            allLectures = allLectures.concat(data);
+        }
+    });
+
+    this.allLectures = allLectures;
+    this.displayLectures = [...allLectures];
 
     if (this.allLectures.length === 0) {
         console.info("현재 등록된 강의 데이터가 없습니다.");
